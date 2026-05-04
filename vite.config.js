f64ca95c5993392231a44ead047e1c58de6aeced
smert_v_nishite <darkvespen@gmail.com>
2026-05-04 21:45:36 +0500

    add 80

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '178.238.225.239',
+    port: 80
+  }
 })
